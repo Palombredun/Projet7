e5edb84d0c9308655dd0f8a02c48d8f1222d9e92
Palombredun <baptiste.fina@hotmail.com>
2018-11-06 16:37:05 +0100

    commit après bug écran noir

--- ocapp/views.py
+++ ocapp/views.py
@@ -31,22 +31,23 @@ def results():
             textBot = result[2]
             linkWiki = result[3]
             API_KEY = result[4]
-            with open("ocapp/templates/map.html", "r") as f:
-                text = f.read()
-            with open("../..temp.html", "w") as g:
-                g.write(text.format(\
-                    textBot=textBot, linkWiki=linkWiki,\
-                    latitude=lat, longitude=lng, API_KEY=API_KEY))
-            #return jsonify({'res': render_template('gmap.html', textBot=textBot, linkWiki=linkWiki, \
-            #    latitude=lat, longitude=lng, API_KEY=API_KEY)})
-            return '0'
+            script_src = "https://maps.googleapis.com/maps/api/js?key=" + API_KEY +"&callback=initMap"
+            #with open("ocapp/templates/map.html", "r") as f:
+            #    text = f.read()
+            #with open("../..temp.html", "w") as g:
+            #    g.write(text.format(\
+            #        textBot=textBot, linkWiki=linkWiki,\
+            #        latitude=lat, longitude=lng, API_KEY=API_KEY)\
+            #    )
+            return render_template('map.html', textBot=textBot, linkWiki=linkWiki, \
+                latitude=lat, longitude=lng, script_src=script_src)
         else:
-            with open("ocapp/templates/error.html", "r") as f:
-                text = f.read()
-            with open("ocapp/templates/temp.html", "w") as g:
-                g.write(text.format(error_message=result))
-            #return render_template('error.html', error_message=result)
-            return '0'
+            #with open("ocapp/templates/error.html", "r") as f:
+            #    text = f.read()
+            #with open("ocapp/templates/temp.html", "w") as g:
+            #    g.write(text.format(error_message=result))
+            message = "Désolé mon petit, je n'ai rien trouvé. Es-tu sûr de l'orthographe ?"
+            return render_template('error.html', error_message=message)
 
 if __name__ == "__main__":
     app.run()
